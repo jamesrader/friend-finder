941233e74b7f28b65202e85eaeecb4d2f0c25408
USCjames <james.rader@cox.net>
2019-05-08 12:13:03 -0700

    Visual updates, data entered for potential matches.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,39 +1,38 @@
-
 var data = [
   {
     name: "Spongebob Squarepants",
     photo: "../images/spongebob2.png",
-    scores: []
+    scores: [5, 5, 3, 5, 4, 1, 4, 3, 3, 1]
   },
   {
     name: "Patrick Star",
     photo: "../images/spongebob2.png",
-    scores: []
+    scores: [1, 3, 3, 5, 1, 2, 5, 2, 5, 1]
   },
   {
     name: "Squidward Tentacles",
     photo: "../images/squidward.png",
-    scores: []
+    scores: [3, 1, 1, 5, 1, 3, 1, 5, 4, 3]
   },
   {
     name: "Mr. Krabs",
     photo: "../images/krabs.png",
-    scores: []
+    scores: [3, 4, 2, 5, 1, 5, 2, 3, 4, 4]
   },
   {
     name: "Sandy Cheeks",
     photo: "../images/sandy.png",
-    scores: []
+    scores: [3, 3, 5, 2, 1, 1, 2, 4, 2, 3]
   },
   {
     name: "Plankton",
     photo: "../images/plankton.png",
-    scores: []
+    scores: [4, 4, 1, 5, 1, 4, 2, 2, 4, 2]
   },
   {
     name: "Gary",
     photo: "../images/gary.png",
-    scores: []
+    scores: [1, 1, 1, 5, 5, 1, 5, 1, 5, 3]
   }
 ];
 
